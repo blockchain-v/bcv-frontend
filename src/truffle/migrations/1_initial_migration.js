6675d0ff8e7f830b239dbd1b1cbe9a5b93221663
PKiechl <pascal.kiechl@bluewin.ch>
2021-11-07 17:03:41 +0100

    working contract call & service.

--- src/truffle/migrations/1_initial_migration.js
+++ src/truffle/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const VNFDeployment = artifacts.require("VNFDeployment");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(VNFDeployment);
 };
--- src/views/ContractInterface.vue
+++ src/views/ContractInterface.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="contract-interface">
     <h1>TEST TEST TEST</h1>
+    <button @click="performTestCall">click here for large boom</button>
   </div>
 </template>
 
 <script>
+import { VNFContract } from "../services/truffleService";
+
 export default {
   name: "ContractInterface",
-  methods: {},
+  methods: {
+    async performTestCall() {
+      console.log("registered button press, performing call...");
+      console.log("contract data:", VNFContract);
+
+      const accounts = await window.web3.eth.getAccounts();
+      const account = accounts[0];
+
+      console.log("accs:", accounts);
+
+      const registerRequest = VNFContract.methods.register("jaldsfjaslkdfalkj");
+      const registerResult = await registerRequest.send({
+        from: account,
+        gasPrice: window.web3.gasPrice,
+        gasLimit: "500000",
+      });
+
+      console.log("resigterResult", registerResult);
+    },
+  },
 };
 </script>
 
